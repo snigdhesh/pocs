ddb69b2459f88fe6b1dc158b9d9c7c33f1594799
Naga Vadlapudi <NagaSnigdhesh.Vadlapudi1@T-Mobile.com>
2018-12-26 14:01:15 -0500

    Addin environmental details functionality

--- thymeleaf-demo/src/main/java/com/home/thymeleafdemo/controllers/WelcomeController.java
+++ thymeleaf-demo/src/main/java/com/home/thymeleafdemo/controllers/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.home.thymeleafdemo.controllers;
 
+import com.home.thymeleafdemo.model.Environment;
+import com.home.thymeleafdemo.model.EnvironmentModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -25,6 +29,32 @@ public class WelcomeController {
         model.put("appName",appName);
         model.put("appVersion",appVersion);
         return "welcome";
+    }
+
+    @RequestMapping("/environments")
+    public String getEnvs(Map<String,Object> model){
+
+
+
+        Environment devEnvironment=new Environment();
+        devEnvironment.setName("dev");
+        devEnvironment.setUrl("dev.com");
+
+        Environment testEnvironment=new Environment();
+        testEnvironment.setUrl("test.com");
+        testEnvironment.setName("test");
+
+        List<Environment> environmentList=new ArrayList<>();
+        environmentList.add(devEnvironment);
+        environmentList.add(testEnvironment);
+
+        EnvironmentModel environmentModel=new EnvironmentModel();
+        environmentModel.setEnvironmentList(environmentList);
+
+        model.put("environmentModel",environmentModel);
+
+
+        return "environments";
     }
 
 }
